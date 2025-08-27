5771621f6358b0c228d0e4fff99ea10f4385f705
Vinicius dos Santos <viniciusreis.pro@gmail.com>
2025-08-27 10:40:27 -0300

    commit para entrega

--- js/compare.js
+++ js/compare.js
@@ -3,10 +3,10 @@
 let carArr = [];
 
 class Car {
-   
 
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-        this.nome = nome; 
+
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image) {
+        this.nome = nome;
         this.preco = preco;
         this.alturaCacamba = alturaCacamba;
         this.alturaVeiculo = alturaVeiculo;
@@ -18,73 +18,75 @@ class Car {
         this.roda = roda;
         this.image = image;
     }
-} 
+}
 
 // search on array if exist carClass returning 1 if not return -1
 function GetCarArrPosition(arr, carClass) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].nome === carClass.nome)
             return i;
     }
     return -1;
 }
 
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        if(el.checked){
-            if (carArr.length >= 2){
+
+    if (carClass instanceof Car) {
+        if (el.checked) {
+            if (carArr.length >= 2) {
                 el.checked = false;
-                alert("Você só pode comparar dois veículos por cada vez");
+                alert("Você só pode comparar 2 veículos por cada vez!");
                 return;
             }
-            if(GetCarArrPosition(carArr, carClass) === -1){
+            if (GetCarArrPosition(carArr, carClass) === -1) {
                 carArr.push(carClass);
             }
-            
+
         } else {
             const index = GetCarArrPosition(carArr, carClass);
-            if(index !== -1){
+            if (index !== -1) {
                 carArr.splice(index, 1);
             }
-        } 
+        }
         botaoComparar();
     } else {
         throw "You need set a Car Class";
     }
 }
 
-function botaoComparar(){
-    const compareBtn =  document.querySelector("button[onclick=1'ShowCompare()']");
+function botaoComparar() {
+    const compareBtn = document.querySelector("button[onclick=1'ShowCompare()']");
 
-    if(carArr.length === 2){
-        compareBtn.disable = false;
+    if (carArr.length === 2) {
+        compareBtn.disabled = false;
         compareBtn.style.opacity = 1;
         compareBtn.style.cursor = "pointer";
     } else {
-        compareBtn.disable = true;
+        compareBtn.disabled = true;
         compareBtn.style.opacity = 0.5;
-        compareBtn.style.cursor = "not-allowed";
+        compareBtn.style.cursor = "not-allowed"
     }
 }
 
 function ShowCompare() {
-    if(carArr.length < 2) {
+    if (carArr.length < 2) {
         alert("Precisa marcar 2 carros para apresentar a comparação");
         return;
     }
 
     UpdateCompareTable();
     document.getElementById("compare").style.display = "block";
+    document.body.classList.add("compare-open")
 }
 
-function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
+function HideCompare() {
+    document.getElementById("compare").style.display = "none";
+    document.body.classList.remove("compare-open");
 }
 
 function UpdateCompareTable() {
-    
-    for(let i = 0; i < carArr.length; i++){
+
+    for (let i = 0; i < carArr.length; i++) {
         const car = carArr[i];
 
         document.getElementById(`compare_image_${i}`).innerHTML = `<img src="${car.image}" width="100">`;
@@ -92,14 +94,17 @@ function UpdateCompareTable() {
         document.getElementById(`compare_modelo_${i}`).textContent = car.nome;
         document.getElementById(`compare_alturacacamba_${i}`).textContent = car.alturaCacamba;
         document.getElementById(`compare_alturaveiculo_${i}`).textContent = car.alturaVeiculo;
-        document.getElementById(`compare_alturasolo_${î}`).textContent = car.alturaSolo;
+        document.getElementById(`compare_alturasolo_${i}`).textContent = car.alturaSolo;
         document.getElementById(`compare_capacidadecarga_${i}`).textContent = car.capacidadeCarga;
         document.getElementById(`compare_motor_${i}`).textContent = car.motor;
         document.getElementById(`compare_potencia_${i}`).textContent = car.potencia;
         document.getElementById(`compare_volumecacamba_${i}`).textContent = car.volumeCacamba;
         document.getElementById(`compare_roda_${i}`).textContent = car.roda;
         document.getElementById(`compare_preco_${i}`).textContent = `R$ ${car.preco.toLocaleString('pt-BR')}`;
-        
-
     }
+
+    
 }
+document.addEventListener('DOMContentLoaded', function(){
+    botaoComparar();
+})
--- js/form.js
+++ js/form.js
@@ -2,26 +2,28 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, email, telefone, tipo, mensagem){
+        this.nome = nome;   
+        this.email = email;
+        this.telefone = telefone;
+        this.tipo = tipo;
+        this.mensagem = mensagem;
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+    let data = new contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("email").value, 
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("tipo").value,
+        form.elements.namedItem("mensagem").value);
+
+    console.log("dados do formulário:", data);
+
+    alert(`Obrigado, ${data.nome}! Sua mensagem de ${data.tipo}, foi enviada!`)
+
+    form.reset();
 }
 
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
-}
